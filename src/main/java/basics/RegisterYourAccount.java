3f99e923c0f28c12062be8e7596a96f093c6925e
RajvinderKaur <rajvindergahlla91@gmail.com>
2026-03-17 18:47:07 -0400

    selenium code for registering account

--- src/main/java/basics/RegisterYourAccount.java
+++ src/main/java/basics/RegisterYourAccount.java
@@ -1,41 +1,70 @@
 package basics;
 
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import net.datafaker.Faker;
+
 public class RegisterYourAccount {
 
 	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();     // open browser
-		driver.get("https://with-bugs.practicesoftwaretesting.com/#/");
+		Faker fakename=new Faker();
+		
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+		String DOB=sdf.format(fakename.date().birthday());
+		
+		WebDriver driver = new ChromeDriver();  
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+	    driver.get("https://with-bugs.practicesoftwaretesting.com/#/");
+	    
 		WebElement signInEle=driver.findElement(By.xpath("//a[text()='Sign in']"));
 		signInEle.click();
+		
 		WebElement registerYourAccEle=driver.findElement(By.xpath("//a[text()='Register your account']"));
 		registerYourAccEle.click();
+		
 		WebElement firstNameEle =driver.findElement(By.xpath("//input[@id='first_name']"));
-		firstNameEle.sendKeys("Rajvinder");
+		firstNameEle.sendKeys(fakename.name().firstName());
+		
 		WebElement lastNameEle =driver.findElement(By.xpath("//input[@id='last_name']"));
-		lastNameEle.sendKeys("kaur");
+		lastNameEle.sendKeys(fakename.name().lastName());
+		
 		WebElement dOBEle =driver.findElement(By.xpath("//input[@id='dob']"));
-		dOBEle.sendKeys("1991-04-10");
+		dOBEle.sendKeys(DOB);
+		
 		WebElement addressEle =driver.findElement(By.xpath("//input[@id='address']"));
-		addressEle.sendKeys("fitzgibson street");
+		addressEle.sendKeys(fakename.address().fullAddress());
+		
 		WebElement postCodeEle =driver.findElement(By.xpath("//input[@id='postcode']"));
-		postCodeEle.sendKeys("L6y5y2");
+		postCodeEle.sendKeys(fakename.address().zipCode());
+		
 		WebElement cityEle =driver.findElement(By.xpath("//input[@id='city']"));
-		cityEle.sendKeys("Ontario");
+		cityEle.sendKeys(fakename.address().city());
+		
 		WebElement countryEle=driver.findElement(By.xpath("//input[@id='state']"));
-		countryEle.sendKeys("canada");
-//		WebElement stateEle=driver.findElement(By.xpath("//select[@id='country']"));
-//		stateEle.sendKeys("Ontario");
-		WebElement phoneEle=driver.findElement(By.xpath("//input[@id='phone']"));
-		phoneEle.sendKeys("9059933467");
+		countryEle.sendKeys(fakename.address().country());
+		
+        WebElement phoneEle=driver.findElement(By.xpath("//input[@id='phone']"));
+		phoneEle.sendKeys(fakename.phoneNumber().cellPhone());
+		
 		WebElement eMailEle=driver.findElement(By.xpath("//input[@id='email']"));
-		eMailEle.sendKeys("raj123@gmail.com");
+		eMailEle.sendKeys(fakename.internet().emailAddress());
+		
 		WebElement passwordEle=driver.findElement(By.xpath("//input[@id='password']"));
-		passwordEle.sendKeys("raj123");
+		passwordEle.sendKeys(fakename.internet().password());
+		
+		WebElement eyeIconEle=driver.findElement(By.xpath("//i[@class='fa fa-eye']"));
+		eyeIconEle.click();
+		
+		WebElement registerButtonEle=driver.findElement(By.xpath("//div/button[ contains (text(), 'Register')]"));
+		registerButtonEle.click();
 		
 
 
