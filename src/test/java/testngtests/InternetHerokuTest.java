9e7bc0c98098813483f0b4f884bbb3806e7efa9c
RajvinderKaur <rajvindergahlla91@gmail.com>
2026-03-23 14:34:56 -0400

    InternetHeroku final test code

--- src/test/java/testngtests/InternetHerokuTest.java
+++ src/test/java/testngtests/InternetHerokuTest.java
@@ -15,45 +15,35 @@ public class InternetHerokuTest {
 	@Test
 	public void dynamicControlTest() {
 		WebDriver driver = new ChromeDriver();
-		driver.navigate().to("https://the-internet.herokuapp.com/"); // Go to https://the-internet.herokuapp.com/
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Dynamic Controls']"))).click(); // Click
-																													// on
-																													// Dynamic
-																													// Controls
-																													// link
-
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='text']")));// Wait for the
-																										// disabled text
-																										// field to
-																										// appear under
-																										// Enable/disable
-																										// section
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Enable']"))).click();// click on
-																											// Enable
-																											// button
-		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//button[text()='Enable']")));// Wait until
-																											// Enable
-																											// button is
-																											// disappeared
-		// wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[text()='Disable']")));//Validate
-		// that "Disable" button appears
+		// Go to https://the-internet.herokuapp.com/
+		driver.navigate().to("https://the-internet.herokuapp.com/"); 
+		
+		// Click on dynamic controls link
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Dynamic Controls']"))).click(); 
+		
+		//wait for the disabled text field to appear under Enable/Disable section
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='text']")));
+		
+		//click on Enable button
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Enable']"))).click();
+		
+		//wait until Enable button is disappeared
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//button[text()='Enable']")));
+		
+		// validate that "Disable" button appears is clickable
 		WebElement disableBtnEle = wait
-				.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Disable']")));// is clickable
+				.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Disable']")));
 
-		if (disableBtnEle.isEnabled() && disableBtnEle.isDisplayed()) {
+		Assert.assertTrue(disableBtnEle.isEnabled() && disableBtnEle.isDisplayed(),"Disable button is not clickable") ;
 			System.out.println("Disable button appears and is clickable");
-		}
-
+		
+       //validate that the "It's enabled!" message is displayed
 		WebElement messageHeading = wait
 				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[@id='message']")));
 		String heading = messageHeading.getText();
 
-		Assert.assertEquals(heading, "It's enabled!", "message not displayed successfully");// Validate that "It's
-																							// enabled!" message is
-																							// displayed
-
+		Assert.assertEquals(heading, "It's enabled!", "message not displayed successfully");
 		System.out.println("message displayed successfully ");
 
 		driver.quit();
@@ -63,11 +53,13 @@ public class InternetHerokuTest {
 	public void dynamicControlTest2()
 	{
 		WebDriver driver = new ChromeDriver();
-		driver.navigate().to(" https://the-internet.herokuapp.com/");//Go to https://the-internet.herokuapp.com/
-		
 		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));  
 		
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Dynamic Controls']"))).click();   //Click on Dynamic Controls link
+		//Go to https://the-internet.herokuapp.com/
+		driver.navigate().to(" https://the-internet.herokuapp.com/");
+		
+		//Click on Dynamic Controls link
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Dynamic Controls']"))).click();   
 		
 		//Wait for the Remove button to be clickable under Remove/add section
 		WebElement removeButtonEle =wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'Remove')]")));
@@ -85,11 +77,13 @@ public class InternetHerokuTest {
 		WebElement addButtonEle=wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Add']")));
 		Assert.assertTrue(addButtonEle.isDisplayed() && addButtonEle.isEnabled(),
                 "Add button is not displayed or clickable!");
+		System.out.println("Remove button is removed and Add button is displayed successfully");
 		
 		//Validate that "It's gone!" message is displayed
 		WebElement messageHeadingEle=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[@id='message']")));
 		String head =messageHeadingEle.getText();
 		Assert.assertEquals(head, "It's gone!", "message not displayed successfully");
+		System.out.println("Its gone is displayed successfully");
 		driver.quit();
 			
 		
