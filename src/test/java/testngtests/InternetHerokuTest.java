c1b62584d40e4c975aeba9692d7097b2d0c58089
RajvinderKaur <rajvindergahlla91@gmail.com>
2026-03-22 20:51:45 -0400

    code for Dynamic Control

--- src/test/java/testngtests/InternetHerokuTest.java
+++ src/test/java/testngtests/InternetHerokuTest.java
@@ -11,37 +11,29 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-//Test: Validate the user can enable the text field on Dynamic Control Page
-//
-//Steps:
-//
-//1. Go to https://the-internet.herokuapp.com/
-//2. Click on Dynamic Controls link
-//3. Wait for the disabled text field to appear under Enable/disable section
-//4. Click on Enable button 
-//5. Wait until Enable button is disappeared, Validate that "Disable" button appears and is clickable  (Note: you need to use three different exp wait methods here)
-//6. Validate that "It's enabled!" message is displayed
+
 
 public class InternetHerokuTest {
 	@Test
 	public void dynamicControlTest()
 	{
 		WebDriver driver = new ChromeDriver();
-		driver.navigate().to("https://the-internet.herokuapp.com/");
+		driver.navigate().to("https://the-internet.herokuapp.com/");      //Go to https://the-internet.herokuapp.com/
 		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
 		
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Dynamic Controls']"))).click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Dynamic Controls']"))).click();   //Click on Dynamic Controls link
 		
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='text']")));
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Enable']"))).click();
-		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//button[text()='Enable']")));
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[text()='Disable']")));
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Disable']")));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='text']")));//Wait for the disabled text field to appear under Enable/disable section
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Enable']"))).click();//click on Enable button 
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//button[text()='Enable']")));//Wait until Enable button is disappeared
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[text()='Disable']")));//Validate that "Disable" button appears
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Disable']")));//is clickable
 		System.out.println("Disable button appears and is clickable");
 		
 		WebElement messageHeading = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[@id='message']")));
 		String heading= messageHeading.getText();
-		Assert.assertEquals(heading,"It's enabled!");
+		Assert.assertEquals(heading,"It's enabled!");//Validate that "It's enabled!" message is displayed
+
 		System.out.println("required message displayed successfully ");
 		driver.quit();
 		}
