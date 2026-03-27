3417bb8c414891c3de923e4a577ec7d63bd1445b
RajvinderKaur <rajvindergahlla91@gmail.com>
2026-03-26 22:31:20 -0400

    Selenium Alerts Testing

--- src/test/java/testngtests/AlertClassTest.java
+++ src/test/java/testngtests/AlertClassTest.java
@@ -33,17 +33,61 @@ public class AlertClassTest {
 		Assert.assertTrue(alertHead.equals("I am a JS Alert"));
 		alertText.accept();
 		
-		WebElement result =driver.findElement(By.xpath("//p[@id='result']"));
-		String heading =result.getText();
-	
-		Assert.assertEquals(heading,"You successfully clicked an alert");
+		WebElement resultEle =wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[@id='result']")));
+		String heading=resultEle.getText();
+		
+		Assert.assertTrue(heading.equals("You successfully clicked an alert"));
 		driver.quit();
 		
+	}
 
+	@Test	
+	public void	cancelJsConfirmTest()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("https://the-internet.herokuapp.com/");
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+		
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='JavaScript Alerts']"))).click();
+		
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Click for JS Confirm']"))).click();
+		
+	
+		Alert alertText =wait.until(ExpectedConditions.alertIsPresent());
+		String alertHead=alertText.getText();
+		Assert.assertTrue(alertHead.equals("I am a JS Confirm"));
+		alertText.dismiss();
+		
+		WebElement resultEle =wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[@id='result']")));
+		String heading=resultEle.getText();
+		
+		Assert.assertTrue(heading.equals("You clicked: Cancel"));
+		driver.quit();
+	}
+		@Test
+		public void jsPromptTest()
+		{
+			WebDriver driver = new ChromeDriver();
+			driver.navigate().to("https://the-internet.herokuapp.com/");
+			WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+			
+			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='JavaScript Alerts']"))).click();
+			
+			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Click for JS Prompt']"))).click();
+			
+		
+			Alert alertText =wait.until(ExpectedConditions.alertIsPresent());
+			String alertHead=alertText.getText();
+			Assert.assertTrue(alertHead.equals("I am a JS prompt"));
+			alertText.sendKeys("Raj");
+			alertText.accept();
+			
+			
+			WebElement resultEle =wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[@id='result']")));
+			String heading=resultEle.getText();
+			
+			Assert.assertTrue(heading.equals("You entered: Raj"));
+			driver.quit();
+		}
 
-		
-		
-		
-		
-}
 }
