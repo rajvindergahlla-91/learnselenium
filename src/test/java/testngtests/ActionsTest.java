87728010f4a2334c9d16a0b03dfe27efbd018f16
RajvinderKaur <rajvindergahlla91@gmail.com>
2026-03-29 17:57:31 -0400

    drag drop and enter text using Actions Class

--- src/test/java/testngtests/ActionsTest.java
+++ src/test/java/testngtests/ActionsTest.java
@@ -3,6 +3,7 @@ package testngtests;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,24 +13,34 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class ActionsTest {
+public class ActionsTest extends BaseAlert   //used @beforemethod and @aftermethod
+{
 	@Test
 public void DragandDropactionsTest()
 {
-	WebDriver driver = new ChromeDriver();
-	driver.navigate().to("https://the-internet.herokuapp.com/");
-	WebDriverWait wait= new WebDriverWait(driver,Duration.ofSeconds(10));
+
 	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Drag and Drop']"))).click();
 	
 	WebElement boxA =wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='column-a']")));
 	WebElement boxB =wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='column-b']")));
 	
 	
-	Actions act = new Actions(driver);
+	Actions act = new Actions(driver);      // used actions class for drag and drop
 	act.dragAndDrop(boxA, boxB).perform();
 	WebElement header=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='column-b']/header[text()='A']")));
 	String targetHead=header.getText();
 	Assert.assertEquals(targetHead, "A");
 	
 }
+	@Test
+	public void enterActionsData()            // used actions class to enter username
+	
+	{
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Form Authentication']"))).click();
+		Actions act = new Actions(driver);
+		WebElement usernameEle =wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='username']")));
+		act.moveToElement(usernameEle).click().sendKeys("tomsmith").keyDown(Keys.CONTROL).sendKeys("A").keyUp(Keys.CONTROL).perform();
+		
+		
+	}
 }
