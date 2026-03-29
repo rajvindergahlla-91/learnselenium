639c584da7ae7946f59ef51c44b721e0cd07e718
RajvinderKaur <rajvindergahlla91@gmail.com>
2026-03-28 23:50:40 -0400

    alert interface using testng annotations before anf after method

--- src/test/java/testngtests/AlertClassTest.java
+++ src/test/java/testngtests/AlertClassTest.java
@@ -1,34 +1,31 @@
 package testngtests;
 
-import java.time.Duration;
+//import java.time.Duration;
 //import java.util.List;
 
 //import org.jspecify.annotations.Nullable;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+//import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class AlertClassTest {
+public class AlertClassTest extends BaseAlert{
 	@Test
 	public void jsAlertTest()
 	
 	{
-		WebDriver driver = new ChromeDriver();
-		driver.navigate().to("https://the-internet.herokuapp.com/");
-		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
 		
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='JavaScript Alerts']"))).click();
 		
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Click for JS Alert']"))).click();
-		
 	
-		Alert alertText =wait.until(ExpectedConditions.alertIsPresent());
+        Alert alertText=driver.switchTo().alert();
+		//Alert alertText =wait.until(ExpectedConditions.alertIsPresent());
 		String alertHead=alertText.getText();
 		Assert.assertTrue(alertHead.equals("I am a JS Alert"));
 		alertText.accept();
@@ -37,16 +34,13 @@ public class AlertClassTest {
 		String heading=resultEle.getText();
 		
 		Assert.assertTrue(heading.equals("You successfully clicked an alert"));
-		driver.quit();
 		
 	}
 
 	@Test	
 	public void	cancelJsConfirmTest()
 	{
-		WebDriver driver = new ChromeDriver();
-		driver.navigate().to("https://the-internet.herokuapp.com/");
-		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+	
 		
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='JavaScript Alerts']"))).click();
 		
@@ -62,14 +56,11 @@ public class AlertClassTest {
 		String heading=resultEle.getText();
 		
 		Assert.assertTrue(heading.equals("You clicked: Cancel"));
-		driver.quit();
+		
 	}
 		@Test
 		public void jsPromptTest()
 		{
-			WebDriver driver = new ChromeDriver();
-			driver.navigate().to("https://the-internet.herokuapp.com/");
-			WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
 			
 			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='JavaScript Alerts']"))).click();
 			
@@ -87,7 +78,7 @@ public class AlertClassTest {
 			String heading=resultEle.getText();
 			
 			Assert.assertTrue(heading.equals("You entered: Raj"));
-			driver.quit();
+			
 		}
 
 }
---
+++ src/test/java/testngtests/BaseAlert.java
@@ -0,0 +1,33 @@
+package testngtests;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseAlert {
+	WebDriver driver;
+	WebDriverWait wait;
+	
+	@BeforeMethod
+	public void preReqBaseTest()
+	{
+		
+	    System.out.println("I am in before Method");
+		driver = new ChromeDriver();
+		driver.navigate().to("https://the-internet.herokuapp.com/");
+		wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+	}
+	
+	@AfterMethod
+	public void packUptest()
+	{
+		driver.quit();
+		System.out.println("I am in after method");
+	
+
+}
+}
