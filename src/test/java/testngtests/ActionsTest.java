df9b38d00610e2cb645db96b26c8532e3e25fb9e
RajvinderKaur <rajvindergahlla91@gmail.com>
2026-03-29 22:59:13 -0400

    actions test practice

--- src/test/java/testngtests/ActionsTest.java
+++ src/test/java/testngtests/ActionsTest.java
@@ -2,23 +2,26 @@ package testngtests;
 
 import java.time.Duration;
 
+//import java.time.Duration;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+//import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ActionsTest extends BaseAlert   //used @beforemethod and @aftermethod
 {
 	@Test
-public void DragandDropactionsTest()
+public void DragAndDropActionsTest()
 {
-
+    driver.navigate().to("https://the-internet.herokuapp.com/");
 	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Drag and Drop']"))).click();
 	
 	WebElement boxA =wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='column-a']")));
@@ -33,14 +36,54 @@ public void DragandDropactionsTest()
 	
 }
 	@Test
-	public void enterActionsData()            // used actions class to enter username
+	public void enterActionsDataTest()            // used actions class to enter username
 	
 	{
+		driver.navigate().to("https://the-internet.herokuapp.com/");
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Form Authentication']"))).click();
 		Actions act = new Actions(driver);
 		WebElement usernameEle =wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='username']")));
 		act.moveToElement(usernameEle).click().sendKeys("tomsmith").keyDown(Keys.CONTROL).sendKeys("A").keyUp(Keys.CONTROL).perform();
 		
 		
+	}
+	
+	@Test
+	public void hoverAndSelectTest()
+	{
+		driver.navigate().to("https://practice.expandtesting.com/hovers#google_vignette");
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollTo(0, document.body.scrollHeight/4)");
+		
+		 WebElement picEle =wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//img[@data-testid='img-user-1']")));
+		 picEle.click();
+		 
+		 Actions act= new Actions(driver);
+		 
+		 
+		act.moveToElement(picEle).pause(Duration.ofSeconds(3)).perform();
+		WebElement optionEle =wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(@href,'/users/1')]")));
+		act.moveToElement(optionEle).pause(Duration.ofSeconds(3)).click().perform();
+		optionEle.click();
+		
+		 WebElement headerText= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2[text()='Welcome user1 ']")));
+		 String heading=headerText.getText();
+		 Assert.assertTrue(heading.contains("Welcome user1"));
+		
+		    
+//		driver.navigate().to("https://www.vinsguru.com/selenium-webdriver-practice-page/");
+//		 WebElement devEle =wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Development']")));
+//		 
+//		 Actions act= new Actions(driver);
+//		 
+//		 
+//		act.moveToElement(devEle).perform();
+//		WebElement optionEle =wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Spring Boot']")));
+//		act.moveToElement(optionEle).click().perform();
+//		
+//		 WebElement headerText= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[@class='entry-title']")));
+//		 String heading=headerText.getText();
+//		 Assert.assertTrue(heading.contains("Spring Boot"));
+//		
 	}
 }
