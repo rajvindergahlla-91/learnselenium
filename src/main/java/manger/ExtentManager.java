8021b8353d3ba42c0450cb5f8b31e662d7b432d8
RajvinderKaur <rajvindergahlla91@gmail.com>
2026-04-11 23:46:17 -0400

    Extent manager report code

---
+++ src/main/java/manger/ExtentManager.java
@@ -0,0 +1,37 @@
+package manger;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+import utils.BaseUtils;
+
+public class ExtentManager {
+
+	public static ExtentReports extent;
+	public static ExtentSparkReporter spark;
+	
+	public static void initReport() throws IOException
+	{
+		String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy hh.mm.ss a"));
+		
+		spark = new ExtentSparkReporter("AutomationReport_" + time + ".html");
+		
+		extent = new ExtentReports();
+		extent.attachReporter(spark);
+		
+		spark.config().setDocumentTitle(BaseUtils.getConfigValue("documentTitle"));
+		spark.config().setReportName(BaseUtils.getConfigValue("reportName"));
+		spark.config().setTheme(Theme.STANDARD);
+		
+	}
+	
+	public static void flushReport()
+	{
+		extent.flush();
+	}
+}
