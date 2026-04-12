9fa1998f11094f0088afeeb806bfaefbc45c7e21
RajvinderKaur <rajvindergahlla91@gmail.com>
2026-04-12 00:54:33 -0400

    Extent reports part

--- src/main/java/pageclasses/WelcomeToTheInternetPage.java
+++ src/main/java/pageclasses/WelcomeToTheInternetPage.java
@@ -2,6 +2,7 @@ package pageclasses;
 
 import org.openqa.selenium.By;
 
+import manger.ExtentTestManager;
 import utils.UTActionsUtils;
 
 
@@ -16,21 +17,28 @@ public class WelcomeToTheInternetPage {
 	public void clickFormAuthentication() {
 
 		UTActionsUtils.click(formAuth);
+		ExtentTestManager.log.info("clicked Form Authentication");
 	}
 
 	public String getFormAuthPageTitle() {
+		
 
-		return UTActionsUtils.getText(heading);
+		String titleText=UTActionsUtils.getText(heading);
+		ExtentTestManager.log.info("Title text");
+		return titleText;
 	}
 
 	public void clickDragAndDrop() {
 		UTActionsUtils.click(dragDrop);
+		ExtentTestManager.log.info("clicked Drag and Drop");
 	}
 
 	public void clickContextMenu() {
 		UTActionsUtils.click(contextMenu);
+		ExtentTestManager.log.info("clicked context menu");
 	}
     public void clickJavaScriptAlerts() {
     	UTActionsUtils.click(javaScript);
+    	ExtentTestManager.log.info("clicked javaScript Alerts");
     }
 }
--- src/test/java/theinternetherokutest/BaseTest.java
+++ src/test/java/theinternetherokutest/BaseTest.java
@@ -1,18 +1,29 @@
 package theinternetherokutest;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 import manger.DriverManager;
+import manger.ExtentManager;
+import manger.ExtentTestManager;
 import utils.BaseUtils;
 import utils.WaitUtils;
 
 public class BaseTest {
+	@BeforeSuite
+	public void initialise() throws IOException
+	{
+		ExtentManager.initReport();
+	}
 
 	@BeforeMethod
-	public void preReqBaseTest() throws NumberFormatException, IOException {
+	public void preReqBaseTest(Method method) throws NumberFormatException, IOException {
+		ExtentTestManager.createTest(method.getName());
 		DriverManager.initDriver();
 		WaitUtils.initWait();
 		DriverManager.goToUrl(BaseUtils.getConfigValue("url"));
@@ -22,5 +33,10 @@ public class BaseTest {
 	public void packUptest() {
 		DriverManager.quitDriver();
 
+	}
+	@AfterSuite
+	public void tearDown()
+	{
+		ExtentManager.flushReport();
 	}
 }
