2ccef48ebade0b27ccc6fd6d10b21cac032b48c1
RajvinderKaur <rajvindergahlla91@gmail.com>
2026-04-17 23:35:39 -0400

    logic code

--- src/main/java/basics/SumOfNumbersInString.java
+++ src/main/java/basics/SumOfNumbersInString.java
@@ -7,7 +7,7 @@ public class SumOfNumbersInString {
 		int sum = 0;
 
 		for (int i = 0; i < str.length(); i++) {
-			char ch = str.charAt(i);
+			char ch= str.charAt(i);
 
 			if (Character.isDigit(ch))
 
