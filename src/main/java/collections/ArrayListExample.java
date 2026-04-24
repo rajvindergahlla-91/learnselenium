49f950d5c20b78037fc4f89dcfedbd86fde69ffd
RajvinderKaur <rajvindergahlla91@gmail.com>
2026-04-23 22:16:37 -0400

    collections with CRUD operations

--- src/main/java/collections/ArrayListExample.java
+++ src/main/java/collections/ArrayListExample.java
@@ -1,37 +1,38 @@
 package collections;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ArrayListExample {
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
+		// List<String> list = new ArrayList<>();
 		ArrayList<String> aList = new ArrayList<>();
-		
+
 		// create
-		
+
 		aList.add("Puneet");
-		aList.add("Riya");
+		aList.add("Riya");                     // Takes Duplicates
 		aList.add("Minarv");
 		aList.add("vitaharya");
-		aList.add("Asha");
-		
+		aList.add("Asha");                        // maintains order
+		aList.add("Riya");
+
 		// Read
-		System.out.println(" The name of 3rd member is : " + aList.get(3));
-		
+		System.out.println(" The name of 3rd member is : " + aList.get(5));
+
 		// Update
-		
-		aList.set(4,"Divya");
-		System.out.println(aList);
-		
-		
+
+		aList.set(4, "Divya");
+		System.out.println(aList);                // doesnot gives Null
+
 		// Delete
 		aList.remove(4);
 		System.out.println(aList);
-		
+
 		// Read all elements
-		for ( String name: aList)
+		for (String name : aList)
 			System.out.println(name);
-			
+
 	}
 }
---
+++ src/main/java/collections/LinkedListExample.java
@@ -0,0 +1,38 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class LinkedListExample {
+
+	public static void main(String[] args) {
+		// List<String> list = new LinkedList<>();
+		LinkedList<String> lList = new LinkedList<>();
+
+		// create
+
+		lList.add("Puneet");
+		lList.add("Riya");
+		lList.add("Minarv");                // takes duplicate
+		lList.add("vitaharya");
+		lList.add("Asha");                  // maintains order
+		lList.add("Minarv");
+
+		// Read
+		System.out.println(" The name of 3rd member is : " + lList.get(3));
+
+		// Update
+
+		lList.set(4, "Divya");
+		System.out.println(lList);
+
+		// Delete
+		lList.remove(3);
+		System.out.println(lList.get(3));
+
+		// Read all elements                    // doesnot gives Null value but shifts the elements towards left
+		for (String name : lList)
+			System.out.println(name);
+
+	}
+}
