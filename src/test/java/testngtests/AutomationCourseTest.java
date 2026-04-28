a9e7b593fc9d2d46ac3e21428593e66c30249628
RajvinderKaur <rajvindergahlla91@gmail.com>
2026-04-27 20:17:47 -0400

    test 7 - sorting part

--- src/test/java/testngtests/AutomationCourseTest.java
+++ src/test/java/testngtests/AutomationCourseTest.java
@@ -1,6 +1,8 @@
 package testngtests;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -14,62 +16,52 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class AutomationCourseTest {
-	
-	By java =By.xpath("//legend[text()='Language']//following::label[text()=' Java']");
-	By javaListOptions=By.xpath("//table//tr[not(contains(@style, 'display: none'))]/td[3]");
 
-	By intermediateOption =By.xpath("//legend[text()='Level']/following::label[text()=' Intermediate']");
-	By advancedOption =By.xpath("//legend[text()='Level']/following::label[text()=' Advanced']");
-	By beginnerListOptions=By.xpath("//table//tr[not(contains(@style, 'display: none'))]/td[4]");
-	
-	By dropDown=By.xpath("//legend[text()='Min enrollments']/following::div[@id='enrollDropdown']");
-	By enrollOps=By.xpath("//table//tr[not(contains(@style, 'display: none'))]/td[5]");
-	By anyOptions= By.xpath("//ul[@class='dropdown-menu']//li[text()='10,000+']");
-	
-	By pythonOp=By.xpath("//legend[text()='Language']//following-sibling::label[3]");
-	By anyOption= By.xpath("//div[@class=\"dropdown-button\"]//following-sibling::ul//li[3]");
-	By pythonTableEle=By.xpath("//table//tr[not(contains(@style, 'display:none'))]/td[3]");
-	By beginnerTableEle=By.xpath("//table//tr[not(contains(@style, 'display:none'))]/td[4]");
-	By enrollTableEle=By.xpath("//table//tr[not(contains(@style, 'display:none'))]/td[5]");
-	
-	
-	By beginner =By.xpath("//legend[text()='Level']/following-sibling::label[1]");
-	By noData=By.xpath("//div[@id='noData']");
-	
-	
-	By resetButton=By.xpath("//button[text()='Reset']");
-	By anyCheckbox=By.xpath("//legend[text()='Language']/following-sibling::label[' Any']/input[@checked]");
-	By allLevelCheckBox=By.xpath("//legend[text()='Level']/following-sibling::label/input[@checked]");
-	By enrollAny=By.xpath("//div[@class='dropdown-button']/span");
-	By rows= By.xpath("//table/tbody/tr");
-	
-	
-	By dropdown=By.xpath("//select[@id='sortBy']");
-	By enrollColoumn=By.xpath("//table/tbody/tr/td[5]");
-	
-	
-	
-	
-	
+	By java = By.xpath("//legend[text()='Language']//following::label[text()=' Java']");
+	By javaListOptions = By.xpath("//table//tr[not(contains(@style, 'display: none'))]/td[3]");
+
+	By intermediateOption = By.xpath("//legend[text()='Level']/following::label[text()=' Intermediate']");
+	By advancedOption = By.xpath("//legend[text()='Level']/following::label[text()=' Advanced']");
+	By beginnerListOptions = By.xpath("//table//tr[not(contains(@style, 'display: none'))]/td[4]");
+
+	By dropDown = By.xpath("//legend[text()='Min enrollments']/following::div[@id='enrollDropdown']");
+	By enrollOps = By.xpath("//table//tr[not(contains(@style, 'display: none'))]/td[5]");
+	By anyOptions = By.xpath("//ul[@class='dropdown-menu']//li[text()='10,000+']");
+
+	By pythonOp = By.xpath("//legend[text()='Language']//following-sibling::label[3]");
+	By anyOption = By.xpath("//div[@class=\"dropdown-button\"]//following-sibling::ul//li[3]");
+	By pythonTableEle = By.xpath("//table//tr[not(contains(@style, 'display:none'))]/td[3]");
+	By beginnerTableEle = By.xpath("//table//tr[not(contains(@style, 'display:none'))]/td[4]");
+	By enrollTableEle = By.xpath("//table//tr[not(contains(@style, 'display:none'))]/td[5]");
+
+	By beginner = By.xpath("//legend[text()='Level']/following-sibling::label[1]");
+	By noData = By.xpath("//div[@id='noData']");
+
+	By resetButton = By.xpath("//button[text()='Reset']");
+	By anyCheckbox = By.xpath("//legend[text()='Language']/following-sibling::label[' Any']/input[@checked]");
+	By allLevelCheckBox = By.xpath("//legend[text()='Level']/following-sibling::label/input[@checked]");
+	By enrollAny = By.xpath("//div[@class='dropdown-button']/span");
+	By rows = By.xpath("//table/tbody/tr");
+
+	By dropdown = By.xpath("//select[@id='sortBy']");
+	By enrollColoumn = By.xpath("//table/tbody/tr/td[5]");
+
 	@Test
-	public void languageJavaTest()
-	{
-		
-		
+	public void languageJavaTest() {
+
 		WebDriver driver = new ChromeDriver();
-	    driver.navigate().to("https://practicetestautomation.com/practice-test-table/");
-	    WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
-	    wait.until(ExpectedConditions.elementToBeClickable(java)).click();
-	    
-	  //List<WebElement> javaList =driver.findElements(javaListOptions);
-	   List<WebElement> javaList= wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(javaListOptions));
-	
-	  for(WebElement list:javaList)
-	  {
-			  String listText =list.getText();
-			  Assert.assertEquals(listText,"Java");
-	  }
-			  System.out.println("found all java options only");
+		driver.navigate().to("https://practicetestautomation.com/practice-test-table/");
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.elementToBeClickable(java)).click();
+
+		// List<WebElement> javaList =driver.findElements(javaListOptions);
+		List<WebElement> javaList = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(javaListOptions));
+
+		for (WebElement list : javaList) {
+			String listText = list.getText();
+			Assert.assertEquals(listText, "Java");
+		}
+		System.out.println("found all java options only");
 //	  if(!listText.equals("Java"))
 //	  {
 //		  System.out.println("found non java option:" + listText);
@@ -78,167 +70,167 @@ public class AutomationCourseTest {
 //	  {
 //		  System.out.println("found all java options:" + listText);
 //	  }
-	  
-	    
-	    driver.quit();
-		
+
+		driver.quit();
+
 	}
+
 	@Test
- public void levelBeginnersTest()
- {
-		
-		
+	public void levelBeginnersTest() {
+
 		WebDriver driver = new ChromeDriver();
-	    driver.navigate().to("https://practicetestautomation.com/practice-test-table/");
-	    WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
-	    wait.until(ExpectedConditions.elementToBeClickable(intermediateOption)).click();
-	    wait.until(ExpectedConditions.elementToBeClickable(advancedOption)).click();
-	   List<WebElement> beginnersList= wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy( beginnerListOptions));
-	   
-	   for(WebElement list : beginnersList )
-	   {
-		   String listOps =list.getText();
-		   Assert.assertEquals(listOps,"Beginner");
-	   }
-	System.out.println("only beginner level is displayed in table");
-	 driver.quit();
-}
+		driver.navigate().to("https://practicetestautomation.com/practice-test-table/");
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.elementToBeClickable(intermediateOption)).click();
+		wait.until(ExpectedConditions.elementToBeClickable(advancedOption)).click();
+		List<WebElement> beginnersList = wait
+				.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(beginnerListOptions));
+
+		for (WebElement list : beginnersList) {
+			String listOps = list.getText();
+			Assert.assertEquals(listOps, "Beginner");
+		}
+		System.out.println("only beginner level is displayed in table");
+		driver.quit();
+	}
+
 	@Test
-	public void minEnrollmentTest()
-	{
-		
-		
-		
+	public void minEnrollmentTest() {
+
 		WebDriver driver = new ChromeDriver();
-	    driver.navigate().to("https://practicetestautomation.com/practice-test-table/");
-	    WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
-	    
-	    wait.until(ExpectedConditions.elementToBeClickable(dropDown)).click();
-	    wait.until(ExpectedConditions.elementToBeClickable(anyOptions)).click();
-	    List<WebElement> list =wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(enrollOps));
-	    
-	    for(WebElement eList: list)
-	    {
-	    	String enrollList =eList.getText();
-	    	Integer value = Integer.valueOf(enrollList);
+		driver.navigate().to("https://practicetestautomation.com/practice-test-table/");
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+		wait.until(ExpectedConditions.elementToBeClickable(dropDown)).click();
+		wait.until(ExpectedConditions.elementToBeClickable(anyOptions)).click();
+		List<WebElement> list = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(enrollOps));
+
+		for (WebElement eList : list) {
+			String enrollList = eList.getText();
+			Integer value = Integer.valueOf(enrollList);
 			Assert.assertTrue(value >= 10000);
-	    	
-	    }
-	    System.out.println("enrollments greater than 10000 are present only");
-	    driver.quit();
+
+		}
+		System.out.println("enrollments greater than 10000 are present only");
+		driver.quit();
 	}
+
 	@Test
-	public void combinedfilterTest()
-	{
-		
-		
-		
+	public void combinedfilterTest() {
+
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("https://practicetestautomation.com/practice-test-table/");
-		WebDriverWait wait= new WebDriverWait(driver,Duration.ofSeconds(10));
-		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
 		wait.until(ExpectedConditions.elementToBeClickable(pythonOp)).click();
-        wait.until(ExpectedConditions.elementToBeClickable(intermediateOption)).click();
-	    wait.until(ExpectedConditions.elementToBeClickable(advancedOption)).click();
-	    wait.until(ExpectedConditions.elementToBeClickable(dropDown)).click();
-	    wait.until(ExpectedConditions.elementToBeClickable(anyOption)).click();
-	    
-	    WebElement python = wait.until(ExpectedConditions.visibilityOfElementLocated(pythonTableEle));
-	    String pythonEle =python.getText();
-	    WebElement begin= wait.until(ExpectedConditions.visibilityOfElementLocated(beginnerTableEle));
-	    String beginEle=begin.getText();
-	    WebElement enrol= wait.until(ExpectedConditions.visibilityOfElementLocated(enrollTableEle));
-	    String enrolEle=enrol.getText();
-	    int value = Integer.valueOf(enrolEle);
-	   
-	    Assert.assertEquals(pythonEle, "Python");
-	    Assert.assertEquals(beginEle, "Beginner");
-	    Assert.assertTrue(value>=10000);
-	   System.out.println("only Python Beginner courses with ≥ 10,000 enrollments are visible");
-	   driver.quit();
-	
-}
+		wait.until(ExpectedConditions.elementToBeClickable(intermediateOption)).click();
+		wait.until(ExpectedConditions.elementToBeClickable(advancedOption)).click();
+		wait.until(ExpectedConditions.elementToBeClickable(dropDown)).click();
+		wait.until(ExpectedConditions.elementToBeClickable(anyOption)).click();
+
+		WebElement python = wait.until(ExpectedConditions.visibilityOfElementLocated(pythonTableEle));
+		String pythonEle = python.getText();
+		WebElement begin = wait.until(ExpectedConditions.visibilityOfElementLocated(beginnerTableEle));
+		String beginEle = begin.getText();
+		WebElement enrol = wait.until(ExpectedConditions.visibilityOfElementLocated(enrollTableEle));
+		String enrolEle = enrol.getText();
+		int value = Integer.valueOf(enrolEle);
+
+		Assert.assertEquals(pythonEle, "Python");
+		Assert.assertEquals(beginEle, "Beginner");
+		Assert.assertTrue(value >= 10000);
+		System.out.println("only Python Beginner courses with ≥ 10,000 enrollments are visible");
+		driver.quit();
+
+	}
+
 	@Test
-	public void noResultsStateTest()
-	{
-		
-		
+	public void noResultsStateTest() {
+
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("https://practicetestautomation.com/practice-test-table/");
-		WebDriverWait wait= new WebDriverWait(driver,Duration.ofSeconds(10));
-		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
 		wait.until(ExpectedConditions.elementToBeClickable(pythonOp)).click();
 		wait.until(ExpectedConditions.elementToBeClickable(beginner)).click();
-	    WebElement text =wait.until(ExpectedConditions.visibilityOfElementLocated(noData));
-	    String textMsg=text.getText();
-	    
-	    Assert.assertTrue(textMsg.contains("No"));
-	    System.out.println("no matching courses for this combination");
-	    driver.quit();
-	}	
+		WebElement text = wait.until(ExpectedConditions.visibilityOfElementLocated(noData));
+		String textMsg = text.getText();
+
+		Assert.assertTrue(textMsg.contains("No"));
+		System.out.println("no matching courses for this combination");
+		driver.quit();
+	}
+
 	@Test
-	public void resetButtonBehaviour()
-	{
-		
-		
-		
+	public void resetButtonBehaviour() {
+
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("https://practicetestautomation.com/practice-test-table/");
-		WebDriverWait wait= new WebDriverWait(driver,Duration.ofSeconds(10));
-	
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
 		wait.until(ExpectedConditions.elementToBeClickable(java)).click();
-	    WebElement buttonText=wait.until(ExpectedConditions.visibilityOfElementLocated(resetButton));
-	    String heading =buttonText.getText();
-	    
-	    Assert.assertEquals(heading,"Reset");
-	    System.out.println("Reset button is visible");
-	    
-	    wait.until(ExpectedConditions.elementToBeClickable(resetButton)).click();
-	    
-	    WebElement checkBox=wait.until(ExpectedConditions.visibilityOfElementLocated(anyCheckbox));
-	    boolean selected=checkBox.isSelected();
-	     Assert.assertTrue(selected);
-	     System.out.println("'Any' checkbox under Language is selected");
-	     
-	     WebElement allCheckBox=wait.until(ExpectedConditions.visibilityOfElementLocated(allLevelCheckBox));
-	     boolean Tr=allCheckBox.isSelected();
-	     
-	     Assert.assertTrue(Tr);
-	     System.out.println("All the checkbox under level are selected");
-	     
-	     WebElement anyField=wait.until(ExpectedConditions.visibilityOfElementLocated(enrollAny));
-	     String textOfField=anyField.getText();
-	
-	     Assert.assertEquals(textOfField, "Any");
-	     System.out.println("'Any' field is visible under enrollment");
-	     
-	    boolean truee= wait.until(ExpectedConditions.invisibilityOfElementLocated(resetButton));
-	    Assert.assertTrue(truee);
-	    System.out.println("reset button is hidden");
-	     
-	    
-		
-		  List<WebElement>allRows=wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(rows)); 
-		  boolean yes= allRows.stream().allMatch(WebElement::isDisplayed);
-		  
-	    Assert.assertTrue(yes);
-	    System.out.println("all rows are visible in table");
-	    driver.quit();
-	 }	
+		WebElement buttonText = wait.until(ExpectedConditions.visibilityOfElementLocated(resetButton));
+		String heading = buttonText.getText();
+
+		Assert.assertEquals(heading, "Reset");
+		System.out.println("Reset button is visible");
+
+		wait.until(ExpectedConditions.elementToBeClickable(resetButton)).click();
+
+		WebElement checkBox = wait.until(ExpectedConditions.visibilityOfElementLocated(anyCheckbox));
+		boolean selected = checkBox.isSelected();
+		Assert.assertTrue(selected);
+		System.out.println("'Any' checkbox under Language is selected");
+
+		WebElement allCheckBox = wait.until(ExpectedConditions.visibilityOfElementLocated(allLevelCheckBox));
+		boolean Tr = allCheckBox.isSelected();
+
+		Assert.assertTrue(Tr);
+		System.out.println("All the checkbox under level are selected");
+
+		WebElement anyField = wait.until(ExpectedConditions.visibilityOfElementLocated(enrollAny));
+		String textOfField = anyField.getText();
+
+		Assert.assertEquals(textOfField, "Any");
+		System.out.println("'Any' field is visible under enrollment");
+
+		boolean truee = wait.until(ExpectedConditions.invisibilityOfElementLocated(resetButton));
+		Assert.assertTrue(truee);
+		System.out.println("reset button is hidden");
+
+		List<WebElement> allRows = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(rows));
+		boolean yes = allRows.stream().allMatch(WebElement::isDisplayed);
+
+		Assert.assertTrue(yes);
+		System.out.println("all rows are visible in table");
+		driver.quit();
+	}
+
 	@Test
-	public void sortByEnrollmentsTest()
-	{
-		
-		
+	public void sortByEnrollmentsTest() {
+
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("https://practicetestautomation.com/practice-test-table/");
-		WebDriverWait wait= new WebDriverWait(driver,Duration.ofSeconds(10));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		WebElement dropDown = wait.until(ExpectedConditions.elementToBeClickable(dropdown));
-		Select dd= new Select(dropDown);
+		Select dd = new Select(dropDown);
 		dd.selectByVisibleText("Enrollments");
-		
-		//List<WebElement> enrollList=wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(enrollColoumn));
-	
-		
+
+		List<WebElement> enrollList = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(enrollColoumn));
+
+		List<Integer> actualList = new ArrayList<>();
+
+		for (WebElement ele : enrollList)
+
+		{
+			String value = ele.getText();
+			actualList.add(Integer.valueOf(value));
+		}
+
+		List<Integer> expectedList = new ArrayList<>(actualList);
+		Collections.sort(expectedList);
+		Assert.assertEquals(actualList,expectedList);
+		System.out.println("sorted correctly");
+
 	}
 }
