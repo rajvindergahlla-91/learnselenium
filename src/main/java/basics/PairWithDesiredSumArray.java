ca866962f6f89112fa9dc84c4bcb1f3e67b12833
RajvinderKaur <rajvindergahlla91@gmail.com>
2026-05-21 22:40:35 -0400

    desired sum pair

--- src/main/java/basics/PairWithDesiredSumArray.java
+++ src/main/java/basics/PairWithDesiredSumArray.java
@@ -2,7 +2,7 @@ package basics;
 
 public class PairWithDesiredSumArray {
 	
-	public static void main(String [] args)
+	/*public static void main(String [] args)
 	{
 		int[] arr = { 2,4,3,7,5,1,6};
 		int target = 7;
@@ -18,4 +18,20 @@ public class PairWithDesiredSumArray {
 		}
 	}
 
+}*/
+	public static void main(String[] args)
+	{
+		int[] arr = {2,3,4,5,1,8,9,7};
+		int target= 9;
+		for(int i=0; i<arr.length;i++)
+		{
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if(arr[i]+arr[j]==target)
+				{
+					System.out.println("the desired pair is :"  + arr[i] + " " + arr[j]);
+				}
+			}
+		}
+	}
 }
