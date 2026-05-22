794096563bbdc85eebb7108263e609a647a5c0b6
RajvinderKaur <rajvindergahlla91@gmail.com>
2026-05-22 14:50:52 -0400

    reversing a string

---
+++ src/main/java/basics/ReverseString.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class ReverseString {
+	public static void main(String[] args)
+	{
+		String str= "Rajvinder";
+		String rev= "";
+		for(int i=str.length()-1; i>=0;i--)
+		{
+			rev= rev + str.charAt(i);
+		}
+		System.out.println(rev);
+	}
+
+}
