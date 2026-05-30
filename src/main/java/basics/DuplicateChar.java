eb5c205d10b6bb32034e7e616263ad62224ce958
RajvinderKaur <rajvindergahlla91@gmail.com>
2026-05-29 23:46:53 -0400

    duplicate character

--- src/main/java/basics/DuplicateChar.java
+++ src/main/java/basics/DuplicateChar.java
@@ -19,7 +19,7 @@ public static void main(String[] args)
 	String str = "programming";
 	char[] arr=str.toCharArray();
 	for(int i=0; i < arr.length; i++) {
-		int count = 1;
+		int count =1;
 		for(int j=i+1; j<arr.length; j++) {
 			if(arr[i]==arr[j]) {
 				count++;
